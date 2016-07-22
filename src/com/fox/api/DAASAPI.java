2eb87df60c6d525537d0029ad20b06d95b98cea4
soafih <rakshitha.shetty@fox.com>
2016-07-22 15:55:23 -0700

    u

--- src/com/fox/api/DAASAPI.java
+++ src/com/fox/api/DAASAPI.java
@@ -21,105 +21,139 @@ import org.apache.http.entity.ContentType;
 import org.apache.http.entity.mime.MultipartEntityBuilder;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import com.fox.api.payload.DaasAPIRequest;
 
-
-
 @Path("/service")
 public class DAASAPI {
 	@POST
 	@Path("/DAASAPI")
-	@Produces({MediaType.APPLICATION_JSON,MediaType.APPLICATION_XML})
-	@Consumes({MediaType.APPLICATION_JSON,MediaType.APPLICATION_XML})
+	@Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+	@Consumes({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
 
+	public Response generateService(DaasAPIRequest request) throws ClientProtocolException, IOException, JSONException {
+		int statusCode = 200;
+		String result = null;
+		CloseableHttpClient httpclient = HttpClients.createDefault();
+		CloseableHttpResponse response2 = null;
+		try {
+			;
+			InputStream is = new ByteArrayInputStream(generateInput(request).getBytes());
 
-	public Response generateService(DaasAPIRequest request) throws ClientProtocolException, IOException, JSONException
-			 {
-	int statusCode =200;
-	 CloseableHttpClient httpclient = HttpClients.createDefault();
-	  CloseableHttpResponse response2 = null;
-	  try{
-		  ;
-		  InputStream is=new ByteArrayInputStream(generateInput(request).getBytes()) ;
-		 
-	  HttpPost httpPost = new HttpPost("http://jenkins-06hw6.10.135.4.49.xip.io/job/DAASBuild/build");
-	  
-	  HttpEntity entity = MultipartEntityBuilder
-			    .create()
-			    .addTextBody("json", "{\"parameter\": [{\"name\":\"app.properties\", \"file\":\"file0\"}]}")
-			    .addBinaryBody("file0",is,ContentType.create("application/octet-stream"), "app.properties").build();
-     
-	  httpPost.setEntity(entity);
-      response2 = httpclient.execute(httpPost);
+			HttpPost httpPost = new HttpPost("http://jenkins-06hw6.10.135.4.49.xip.io/job/DAASBuild/build");
 
- /*    try {
-         System.out.println(response2.getStatusLine());
-       
-         // do something useful with the response body
-         // and ensure it is fully consumed
-         if (response2.getStatusLine().getStatusCode() != 201) {
- 			throw new RuntimeException("Failed : HTTP error code : "
- 				+ response2.getStatusLine().getStatusCode());
- 		}
-       
- 		
+			HttpEntity entity = MultipartEntityBuilder.create()
+					.addTextBody("json", "{\"parameter\": [{\"name\":\"app.properties\", \"file\":\"file0\"}]}")
+					.addBinaryBody("file0", is, ContentType.create("application/octet-stream"), "app.properties")
+					.build();
 
- 		String output;
- 		System.out.println("HI");
- 		System.out.println("Output from Server .... \n");
- 		BufferedReader br = new BufferedReader(
-               new InputStreamReader((response2.getEntity().getContent())));
- 		while ((output = br.readLine()) != null) {
- 			System.out.println(output);
- 		}*/
- 		
- 		JSONObject rootObj = new JSONObject();
-		
-		
-		
-		
-		JSONObject sobj= new JSONObject();
-		//sobj.put("id", info.getQueryParameters().getFirst("id"));
-		//sobj.put("name", info.getQueryParameters().getFirst("name"));
-		
-		sobj.put("result","success");
-		
-		rootObj.put("response", sobj);
-		
-		
-		return Response.status(statusCode).entity(rootObj.toString()).build();
- 	
-     } finally {
-         //response2.close();
-         httpclient.close();
-     }
-	 
- 
-}
-	
-	private String generateInput(DaasAPIRequest request)
-	{
-		
-		StringBuffer sb= new StringBuffer("");
-		sb.append("organization="+request.getOrganization());
-		sb.append("space="+request.getSpace());
-		sb.append("applicationName=" +request.getApplicationName());
-		sb.append("query="+request.getQuery() );
-		sb.append("databaseType="+request.getDatabaseInfo().getDatabaseType() );
-		sb.append("hostName="+request.getDatabaseInfo().getHostName() );
-		sb.append("port="+request.getDatabaseInfo().getPort() );
-		sb.append("databaseName="+request.getDatabaseInfo().getDatabaseName());
-		sb.append("user="+request.getDatabaseInfo().getUser());
-		sb.append("password="+request.getDatabaseInfo().getPassword());
-		sb.append("maxActive="+request.getConnectionAttr().getMaxActive());
-		sb.append("maxIdle="+request.getConnectionAttr().getMaxIdle());
-		sb.append("maxWait="+request.getConnectionAttr().getMaxWait());
-		
+			httpPost.setEntity(entity);
+			response2 = httpclient.execute(httpPost);
+
+			/*
+			 * try { System.out.println(response2.getStatusLine());
+			 * 
+			 * // do something useful with the response body // and ensure it is
+			 * fully consumed if (response2.getStatusLine().getStatusCode() !=
+			 * 201) { throw new RuntimeException("Failed : HTTP error code : " +
+			 * response2.getStatusLine().getStatusCode()); }
+			 * 
+			 * 
+			 * 
+			 * String output; System.out.println("HI"); System.out.println(
+			 * "Output from Server .... \n"); BufferedReader br = new
+			 * BufferedReader( new
+			 * InputStreamReader((response2.getEntity().getContent()))); while
+			 * ((output = br.readLine()) != null) { System.out.println(output);
+			 * }
+			 */
+
+			JSONObject rootObj = new JSONObject();
+
+			JSONObject sobj = new JSONObject();
+			// sobj.put("id", info.getQueryParameters().getFirst("id"));
+			// sobj.put("name", info.getQueryParameters().getFirst("name"));
+
+			sobj.put("result", "success");
+
+			rootObj.put("response", sobj);
+			result = rootObj.toString();
+
+		}
+
+		catch (Exception ex) {
+			JSONObject resultJson = getErrorJson(ex);
+			result = resultJson.toString();
+			statusCode = 501;
+
+		} finally {
+			// response2.close();
+			httpclient.close();
+		}
+		return Response.status(statusCode).entity(result).build();
+
+	}
+
+	private String generateInput(DaasAPIRequest request) throws Exception {
+
+		String dbType = request.getDatabaseInfo().getDatabaseType().toLowerCase();
+		String driverClass;
+		String connectionString;
+
+		if (dbType.equals("oracle")) {
+			driverClass = "oracle.jdbc.driver.OracleDriver";
+			connectionString = "jdbc:oracle:thin:@" + request.getDatabaseInfo().getHostName() + ":"
+					+ request.getDatabaseInfo().getPort() + ":" + request.getDatabaseInfo().getDatabaseName();
+
+		} else if (dbType.equals("AS400")) {
+			driverClass = "com.ibm.as400.access.AS400JDBCDriver";
+			connectionString = "jdbc:as400://" + request.getDatabaseInfo().getHostName() + ";databaseName="
+					+ request.getDatabaseInfo().getDatabaseName();
+
+		}
+
+		else if (dbType.equals("SQLServer")) {
+			driverClass = "com.microsoft.jdbc.sqlserver.SQLServerDriver";
+			connectionString = "jdbc:mysql://" + request.getDatabaseInfo().getHostName() + "/"
+					+ request.getDatabaseInfo().getDatabaseName();
+		}
+
+		else if (dbType.equals("MySQL")) {
+			driverClass = "com.mysql.jdbc.Driver";
+			connectionString = "jdbc:microsoft:sqlserver://" + request.getDatabaseInfo().getHostName() + ":"
+					+ request.getDatabaseInfo().getPort() + ";databaseName="
+					+ request.getDatabaseInfo().getDatabaseName();
+
+		} else {
+			throw new Exception("Database not supported");
+		}
+
+		StringBuffer sb = new StringBuffer("");
+		sb.append("organization=" + request.getOrganization() + "\n");
+		sb.append("space=" + request.getSpace() + "\n");
+		sb.append("applicationName=" + request.getApplicationName() + "\n");
+		sb.append("appHostName=" + request.getApplicationName().toLowerCase() + "\n");
+		sb.append("query=" + request.getQuery() + "\n");
+		sb.append("driverClassName=" + driverClass + "\n");
+		sb.append("ConnectionString=" + connectionString + "\n");
+		sb.append("user=" + request.getDatabaseInfo().getUser() + "\n");
+		sb.append("password=" + request.getDatabaseInfo().getPassword() + "\n");
+		sb.append("maxActive=" + request.getConnectionAttr().getMaxActive() + "\n");
+		sb.append("maxIdle=" + request.getConnectionAttr().getMaxIdle() + "\n");
+		sb.append("maxWait=" + request.getConnectionAttr().getMaxWait() + "\n");
+
 		return sb.toString();
+	}
+
+	private JSONObject getErrorJson(Exception ex) throws JSONException {
+		JSONObject result = new JSONObject();
+		JSONObject childObject = new JSONObject();
+		result.put("Error", childObject);
+		childObject.put("ErrorDetails", ex.getMessage());
+		// childObject.put("Status", "Error");
+		return result;
 	}
 
 }
