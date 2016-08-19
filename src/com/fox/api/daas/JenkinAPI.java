dbccf0b1eb9c6a02bb04ac53e6887564d578dee1
soafih <rakshitha.shetty@fox.com>
2016-08-19 12:19:39 -0700

    Updated the code to capture domain

--- src/com/fox/api/daas/JenkinAPI.java
+++ src/com/fox/api/daas/JenkinAPI.java
@@ -1,7 +1,5 @@
 package com.fox.api.daas;
 
-
-
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 
@@ -12,47 +10,36 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
 
-
-
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import com.fox.api.daas.util.DAASUtility;
 import com.fox.api.daas.util.JenkinUtility;
 
-
-
 @Path("/JenkinUtility")
 public class JenkinAPI {
 	@GET
 	@Path("/GetStatus")
-	@Produces({ MediaType.APPLICATION_JSON})
-	@Consumes({ MediaType.APPLICATION_JSON})
-	
+	@Produces({ MediaType.APPLICATION_JSON })
+	@Consumes({ MediaType.APPLICATION_JSON })
+
 	public Response getStatus_wrapper(@Context UriInfo info) throws JSONException {
 		int statusCode = 200;
 		String result = null;
-		try
-		{
-		String buildNumber = info.getQueryParameters().getFirst("buildNumber");
-		
-		
-		if(buildNumber.trim().equals("0"))
-		{
-			buildNumber = JenkinUtility.getBuildNumber(info.getQueryParameters().getFirst("buildIdentifier"));
-		}
-		
-		 
-		result= JenkinUtility.getStatus(buildNumber).toString();
-		
-		}
-		catch(Exception ex)
-		{
+		try {
+			String buildNumber = info.getQueryParameters().getFirst("buildNumber");
+
+			if (buildNumber.trim().equals("0")) {
+				buildNumber = JenkinUtility.getBuildNumber(info.getQueryParameters().getFirst("buildIdentifier"));
+			}
+
+			result = JenkinUtility.getStatus(buildNumber).toString();
+
+		} catch (Exception ex) {
 			ex.printStackTrace();
 			result = DAASUtility.getErrorJson(ex).toString();
 		}
-		
-		
+
 		return Response.status(statusCode).entity(result).build();
 	}
 
@@ -60,36 +47,34 @@ public class JenkinAPI {
 	@Path("/GetBuildNumber")
 	@Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
 	@Consumes({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
-	
+
 	public Response getBuildNumber_wrapper(@Context UriInfo info) throws JSONException {
 		int statusCode = 200;
 		String result = null;
-		try
-		{
-		
+		try {
+
 			String buildNumber = JenkinUtility.getBuildNumber(info.getQueryParameters().getFirst("buildIdentifier"));
 			JSONObject rootObj = new JSONObject();
 			JSONObject respObj = new JSONObject();
-
 			respObj.put("buildNumber", buildNumber);
+			if (!buildNumber.equals("0")) {
+
+				respObj.put("logURL", System.getenv("JenkinURL") + "/job/" + System.getenv("JenkinJob") + "/"
+						+ buildNumber + "/consoleText");
+			} else {
+				respObj.put("status", "build Not found/Initiated");
+			}
 			rootObj.put("response", respObj);
 
 			result = rootObj.toString();
 		}
-		
-		catch(Exception ex)
-		{
+
+		catch (Exception ex) {
 			ex.printStackTrace();
 			result = DAASUtility.getErrorJson(ex).toString();
 		}
-		
-		
+
 		return Response.status(statusCode).entity(result).build();
 	}
 
-	
-	
-	
-	
-	
 }
